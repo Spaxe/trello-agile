09ac664692cc5258fcb97168a11aa3d4216595f9
Xavier Ho <contact@xavierho.com>
2014-03-10 11:20:00 +1100

    Fixed a bug that didn't remove point sums when the list is empty

--- agile-trello.js
+++ agile-trello.js
@@ -1,36 +1,56 @@
-/* JS */
+/**
+ * Periodic script to make sure points are up to date.
+ * Need more ideas to make Trello better for Agile dev.
+ */
 
-function sumPoints(lists) {
-  for (var j = 0; j < lists.length-1; j++) {
-    var cards = lists[j].querySelectorAll(".list-card-title");
-    var list_title = lists[j].querySelector(".list-header-name");
+;(function AgileTrello(window, document, undefined) {
 
-    var sum = undefined;
-    for (var i = 0; i < cards.length; i++) {
-      var found = cards[i].textContent.match(/^#[0-9]+ \[([0-9]+\.?5?)\]/);
-      if (found) {
-        if (sum === undefined)
-          sum = 0;
-        sum += parseFloat(found[1]);
+
+  function sumPoints (lists) {
+
+    // The last "list" is just "Add a List..." shadow
+    for (var j = 0; j < lists.length-1; j++) {
+      var list = lists[j];
+
+      var list_title = list.querySelector(".list-header-name");
+      var cards = list.querySelectorAll(".list-card-title");
+      var sum = undefined;
+
+      for (var i = 0; i < cards.length; i++) {
+        var card = cards[i];
+
+        var found = card.textContent.match(/^#[0-9]+ \[([0-9]+\.?[0-9]*)\]/);
+        if (found) {
+          if (sum === undefined)
+            sum = 0;
+          sum += parseFloat(found[1]);
+        }
+      }
+
+      var display = getAgileSumDOM(list);
+      var header = list.querySelector(".list-header");
+      header.insertBefore(display, list_title);
+
+      if (sum !== undefined) {
+        display.textContent = "[" + sum + "]";
+      } else {
+        display.textContent = "";
       }
     }
+  }
 
-    var display = lists[j].querySelector(".agile-sum");
+  function getAgileSumDOM (parent) {
+    var display = parent.querySelector(".agile-sum");
     if (display === null) {
       display = document.createElement("span");
       display.className = "agile-sum";
-
-      var header = lists[j].querySelector(".list-header");
-      header.insertBefore(display, list_title);
     }
-
-    if (sum !== undefined) {
-      display.textContent = "[" + sum + "]";
-    }
+    return display;
   }
-}
+
+  window.setInterval(function() {
+    sumPoints(document.querySelectorAll(".list"));
+  }, 500);
 
 
-window.setInterval(function() {
-  sumPoints(document.querySelectorAll(".list"));
-}, 500);
+}(window, document));
